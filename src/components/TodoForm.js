34fd8e317654fba29ccfacb78741ac8fdac9deea
flcsn <flacson98@gmail.com>
2022-03-08 18:56:04 +0800

    added state to Todo Form to clear input values on submit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineSend } from 'react-icons/ai'
 
 const TodoForm = ({ addNewTodo }) => {
+  const [input, setInput] = useState('')
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    addNewTodo(event.target.todo.value)
+    addNewTodo(input)
+    setInput('')
   }
 
   return (
     <div className='container todo-item'>
       <form className='todo-form' onSubmit={handleSubmit}>
-        <input name='todo' type='text' placeholder='This is the Todo Form'/>
+        <input
+          name='todo'
+          type='text'
+          value={input}
+          onChange={({ target }) => setInput(target.value)}
+          placeholder='Type your todos here!'
+          autoComplete='off'
+          autoFocus
+        />
         <div className='btn-container'>
           <button type='submit'>
             <AiOutlineSend />
