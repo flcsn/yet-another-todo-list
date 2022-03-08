00c3a38afd48cd52e354e71477b3cf2afd6aadee
flcsn <flacson98@gmail.com>
2022-03-08 16:22:55 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "node": true,
+      "es6": true
+  },
+  "extends": [ 
+      "eslint:recommended",
+      "plugin:react/recommended"
+  ],
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+      "indent": [
+          "error",
+          2, 
+          { 'SwitchCase': 1 }
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "never"
+      ],
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": [
+          "error", "always"
+      ],
+      "arrow-spacing": [
+          "error", { "before": true, "after": true }
+      ],
+      "react/prop-types": 0
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
