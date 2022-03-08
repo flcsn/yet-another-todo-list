b27e7b93893daa7f8c255d4a482da56e28d7c264
flcsn <flacson98@gmail.com>
2022-03-08 19:12:14 +0800

    clicking todo items now puts a strikethrough

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,16 +2,21 @@ import React from 'react'
 
 const TodoItem = ({ todo, deleteTodo }) => {
   const handleClick = (event) => {
+    event.target.classList.toggle('strikethrough')
+  }
+
+  const handleDelete = (event) => {
     event.preventDefault()
     deleteTodo(todo)
   }
+
   return (
     <div className='todo-item'>
-      <div>
+      <div className='todo-name' onClick={handleClick}>
         {todo.name}
       </div>
       <div className='btn-container'>
-        <button onClick={(event) => handleClick(event)}>
+        <button onClick={(event) => handleDelete(event)}>
           &times;
         </button>
       </div>
